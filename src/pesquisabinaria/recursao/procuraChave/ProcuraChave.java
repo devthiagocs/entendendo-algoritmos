86ac07868756f7509e4f5942d2b16e052c5b2b60
Thiago <thiagocampos413@gmail.com>
2026-08-04 22:17:58 -0300

    Entendendo pilhas e recursão

--- src/pesquisabinaria/recursao/procuraChave/ProcuraChave.java
+++ src/pesquisabinaria/recursao/procuraChave/ProcuraChave.java
@@ -1,4 +1,4 @@
-package pesquisabinaria.recursao;
+package pesquisabinaria.recursao.procuraChave;
 
 import java.util.Stack;
 
---
+++ src/pesquisabinaria/recursao/procuraChave/ProcuraChaveRecursao.java
@@ -0,0 +1,13 @@
+package pesquisabinaria.recursao.procuraChave;
+
+public class ProcuraChaveRecursao { //Recursão é quando o método chama ele mesmo
+    public static void procuraChaveRecursao(Caixa caixa){
+        for (Item item : caixa.getItens()) {
+            if (item.eUmaCaixa()) {
+                procuraChaveRecursao((Caixa) item);
+            } else if (item.eUmaCaixa()){
+                System.out.println("Achei a chave!");
+            }
+        }
+    }
+}
